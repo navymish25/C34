410067edf0cf6dec3ff9201da3f0e026bd340550
navymish25 <navymish25@gmail.com>
2020-07-10 21:41:36 -0400

    datebase usage

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 var ball;
 
+var database,position;
+
 function setup(){
+    database=firebase.database();
+
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballposition = database.ref('ball/position');
+    ballposition.on("value",readPosition, showError);
 }
 
 function draw(){
     background("white");
+    if(position!==undefined){
+
+    
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -21,9 +31,21 @@ function draw(){
         changePosition(0,+1);
     }
     drawSprites();
+    }
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    });
+}
+function readPosition(arg){
+    position=arg.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showError(){
+
 }
